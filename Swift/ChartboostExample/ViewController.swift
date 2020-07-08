31a26290c737e0a8d662f6ca51b85309d9aaa6fd
Arnau Marti Font <ar_marti13@hotmail.com>
2020-07-08 15:21:20 +0200

    corrected main storyboard with a new file

--- Swift/ChartboostExample/ViewController.swift
+++ Swift/ChartboostExample/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showSupport(_ sender: Any) {
+        
+    }
 }
 
