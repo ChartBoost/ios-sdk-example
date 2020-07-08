b55e6f959000c001ad5d3eb60c137c723359d1ee
Arnau Marti Font <ar_marti13@hotmail.com>
2020-07-08 12:55:46 +0200

    base swift project

---
+++ Swift/ChartboostExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChartboostExample
+//
+//  Created by Arnau Marti on 08/07/2020.
+//  Copyright © 2020 Chartboost. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
