ab1cc86a089350459c91cb94e403d626fb80a7ef
Arnau Marti Font <ar_marti13@hotmail.com>
2020-07-08 15:26:56 +0200

    IBActions

--- Swift/ChartboostExample/ViewController.swift
+++ Swift/ChartboostExample/ViewController.swift
@@ -15,17 +15,28 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func cacheInterstitial(_ sender: Any) {
+    }
+
+    @IBAction func showInterstitial(_ sender: Any) {
+    }
+
+    @IBAction func cacheRewarded(_ sender: Any) {
+    }
+
+    @IBAction func showRewarded(_ sender: Any) {
+    }
+
+    @IBAction func showBanner(_ sender: Any) {
+    }
 
     @IBAction func showSupport(_ sender: Any) {
-
         guard let url = URL(string: "http://answers.chartboost.com") else { return }
-
         if #available(iOS 10.0, *) {
             UIApplication.shared.open(url, options: [:], completionHandler: nil)
         } else {
             UIApplication.shared.openURL(url)
         }
-
     }
 }
 
