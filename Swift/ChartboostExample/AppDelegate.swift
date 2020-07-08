91fcb1426dd9047bb3729b565138302ea079afa7
Arnau Marti Font <ar_marti13@hotmail.com>
2020-07-08 14:58:36 +0200

    setting ViewController when after chartboost start

--- Swift/ChartboostExample/AppDelegate.swift
+++ Swift/ChartboostExample/AppDelegate.swift
@@ -15,6 +15,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         print("Chartboost SDK Version ", Chartboost.getSDKVersion() ?? "Error")
+
+        Chartboost.addDataUseConsent(.CCPA(.optInSale))
+        Chartboost.addDataUseConsent(.GDPR(.behavioral))
+
+        Chartboost.setLoggingLevel(.info)
+
+        Chartboost.start(withAppId: "4f21c409cd1cb2fb7000001b",
+                         appSignature: "92e2de2fd7070327bdeb54c15a5295309c6fcd2d") { (success) in
+
+                        self.window = UIWindow(frame: UIScreen.main.bounds)
+                        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                        let vc = storyboard.instantiateViewController(withIdentifier: "viewController")
+                        self.window?.rootViewController = vc
+                        self.window?.makeKeyAndVisible()
+        }
         return true
     }
 
